e858bf310fe9876aa0e557a06c8b3797827010d4
Erbeth Charte <echarte@tutanota.com>
2021-06-11 23:51:24 -0400

    Another generic recursive fns.

--- recursion.erl
+++ recursion.erl
@@ -1,4 +1,22 @@
-map(Fn, []) ->
+-module(recursion).
+-export([lmap/2, fact/1, llength/1, reverse/1]).
+
+lmap(_, []) ->
     [];
-map(Fn, [H|T]) ->
-    [Fn(H)| map(T)].
+lmap(Fn, [H|T]) ->
+    [Fn(H)| lmap(Fn, T)].
+
+fact(0) ->
+    1;
+fact(X) when X > 0 ->
+    X * fact(X-1).
+
+llength([]) ->
+    0;
+llength([_|T]) ->
+    1 + llength(T).
+
+reverse([]) ->
+    [];
+reverse([H|T]) ->
+    reverse(T)++[H].
