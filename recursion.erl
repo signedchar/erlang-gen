0dec614c5374d2ead74444163097aba5d0a57722
Erbeth Charte <echarte@tutanota.com>
2021-06-17 17:41:45 -0400

    Accumulators Public and private functions recursions about case.

--- recursion.erl
+++ recursion.erl
@@ -1,5 +1,5 @@
 -module(recursion).
--export([lmap/2, fact/1, llength/1, reverse/1]).
+-export([lmap/2, fact/1, llength/1, reverse/1, even_and_odds/1]).
 
 lmap(_, []) ->
     [];
@@ -20,3 +20,14 @@ reverse([]) ->
     [];
 reverse([H|T]) ->
     reverse(T)++[H].
+
+even_and_odds(L) ->
+    even_and_odds_acc(L, [], []).
+
+even_and_odds_acc([H|T], Even, Odds) ->
+    case (H rem 2) of
+        0 -> even_and_odds_acc(T, [H|Even], Odds);
+        1 -> even_and_odds_acc(T, Even, [H|Odds])
+    end;
+even_and_odds_acc([], Even, Odds) ->
+    {Even, Odds}.
