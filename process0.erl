ff6438fcaf44ca410f90585caf5263285f04f5fb
Erbeth Charte <echarte@tutanota.com>
2021-06-25 22:16:01 -0400

    Adding generic match Adding area of circle.

--- process0.erl
+++ process0.erl
@@ -11,14 +11,24 @@ loop() ->
             loop();
         {From, {rectangle, Height, Width}} ->
             From ! io:format("Rectangle area is ~p~n", [area({rectangle, Height, Width})]),
+            loop();
+        {From, {circle, Radio}} ->
+            From ! io:format("Circle area is ~p~n", [area({circle, Radio})]),
+            loop();
+        {From, Generic} ->
+            From ! {error, Generic},
             loop()
     end.
 
--spec area({atom(), integer()}) -> integer(); ({atom(), integer(), integer()}) -> integer().
+-spec area({atom(), integer()}) -> integer(); 
+          ({atom(), integer(), integer()}) -> integer();
+          ({atom(), integer()}) -> float().
 area({square, L}) ->
     L * L;
 area({rectangle, Height, Width}) ->
-    Height * Width.
+    Height * Width;
+area({circle, Radio}) ->
+    3.141592654 * Radio * Radio.
 
 client(Pid, Msg) ->
     Pid ! {self(), Msg},
